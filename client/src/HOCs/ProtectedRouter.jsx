e51669e67b4832d0c4d10609f08fb2c124c847d2
Ibragim-Sh <shadiev999@gmail.com>
2025-01-29 17:34:00 +0300

    regaDone

---
+++ client/src/HOCs/ProtectedRouter.jsx
@@ -0,0 +1,6 @@
+import { Navigate, Outlet } from 'react-router-dom';
+function ProtectedRouter({ children, isAllowed, redirectTo }) {
+  if (isAllowed) return <Navigate to={redirectTo} />;
+  return children || <Outlet />;
+}
+export default ProtectedRouter;
