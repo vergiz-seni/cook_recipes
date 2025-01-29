517e5167158bb8edca61cbc8708b39f51fe04103
vergiz-seni <vergizseni@gmail.com>
2025-01-29 17:44:00 +0300

    seeds

---
+++ server/db/migrations/9125922-create-recipe.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Recipes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING,
+      },
+      ingredients: {
+        type: Sequelize.TEXT,
+      },
+      cookingTime: {
+        type: Sequelize.STRING,
+      },
+      img: {
+        type: Sequelize.STRING,
+      },
+      recipe: {
+        type: Sequelize.TEXT,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW'),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW'),
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Recipes');
+  },
+};
---
+++ server/db/models/recipe.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Recipe extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Recipe.init({
+    title: DataTypes.STRING,
+    ingredients: DataTypes.TEXT,
+    cookingTime: DataTypes.STRING,
+    img: DataTypes.STRING,
+    recipe: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'Recipe',
+  });
+  return Recipe;
+};
