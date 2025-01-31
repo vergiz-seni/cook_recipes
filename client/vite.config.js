f4b69bcc190da8e302756b94abad65e9da0b7b29
vergiz-seni <vergizseni@gmail.com>
2025-01-31 16:53:18 +0300

    pre deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,16 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        proxy: {
-            '/api': 'http://localhost:3000',
-        }
-    }
-})
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+  },
+  build: {
+    outDir: '../server/dist',
+  },
+  base: '/',
+});
--- server/src/app.js
+++ server/src/app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const app = express();
+const path = require('path');
 
-const tokenRouter = require('./routes/tokenRouter')
-const authRouter = require('./routes/authRouter')
-const recipeRouter = require('./routes/recipeRouter')
-const favoriteRouter = require('./routes/favoriteRouter')
+const tokenRouter = require('./routes/tokenRouter');
+const authRouter = require('./routes/authRouter');
+const recipeRouter = require('./routes/recipeRouter');
+const favoriteRouter = require('./routes/favoriteRouter');
 
 app.use(cookieParser());
 app.use(express.static('public'));
@@ -14,10 +15,14 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 app.use('/api/auth', authRouter);
 app.use('/api/tokens', tokenRouter);
 app.use('/api/recipes', recipeRouter);
 app.use('/api/favorites', favoriteRouter);
+
+app.use(express.static(path.join(__dirname, '..', 'dist')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
+});
 
 module.exports = app;
